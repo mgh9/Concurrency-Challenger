93fb2feef098157a9f3bd7fb044d09bb16d28e95
ghardashpour-m <ghardashpour@utravs.com>
2025-07-18 21:09:13 +0330

    feat (tester): simulate parallel requests to our API to test Optimistic lock

---
+++ ConcurrencyChallenger.Tester/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConcurrencyChallenger.Tester;
+
+Console.WriteLine("Hello, World!");
+
+// let the API be ready first
+await Task.Delay(5000);
+
+var callAddOrderWithOptimisticLockTasks = TestOptimisticLock.CallAddOrderWithOptimisticLock();
+
+// test# 1 (testing optimistic lock): simulate multiple concurrent requests to our API
+await Task.WhenAll(callAddOrderWithOptimisticLockTasks);
+
+Console.ReadLine();
---
+++ ConcurrencyChallenger.Tester/TestOptimisticLock.cs
@@ -0,0 +1,28 @@
+﻿namespace ConcurrencyChallenger.Tester;
+
+internal class TestOptimisticLock
+{
+    internal static IEnumerable<Task> CallAddOrderWithOptimisticLock()
+    {
+        var tasks = Enumerable.Range(0, 100).Select(i =>
+            Task.Run(async () =>
+            {
+                try
+                {
+                    using var client = new HttpClient();
+                    var request = new HttpRequestMessage(HttpMethod.Post, "https://localhost:7287/api/orders/1");
+                    var response = await client.SendAsync(request);
+                    //response.EnsureSuccessStatusCode();
+
+                    var res = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"[{i}] Status >>>>>> : {response.StatusCode} --- content: `{res}`");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("ERRRROR : " + ex.Message);
+                }
+            })
+        );
+        return tasks;
+    }
+}
