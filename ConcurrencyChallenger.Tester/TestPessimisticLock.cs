0578793439a211e969b41dc91cba8fb87c08875e
ghardashpour-m <ghardashpour@utravs.com>
2025-07-18 21:28:45 +0330

    feat (Tester): add Pessimistic locking test in the console app

---
+++ ConcurrencyChallenger.Tester/TestPessimisticLock.cs
@@ -0,0 +1,28 @@
+﻿namespace ConcurrencyChallenger.Tester;
+
+internal class TestPessimisticLock
+{
+    internal static IEnumerable<Task> CallAddOrderWithTestPessimisticLock()
+    {
+        var tasks = Enumerable.Range(0, 10).Select(i =>
+            Task.Run(async () =>
+            {
+                try
+                {
+                    using var client = new HttpClient();
+                    var request = new HttpRequestMessage(HttpMethod.Post, "https://localhost:7287/api/orders/with-lock/1");
+                    var response = await client.SendAsync(request);
+                    //response.EnsureSuccessStatusCode();
+
+                    var res = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"[{i}] Status >>>>>> : {response.StatusCode} --- content: `{res}`");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("ERRRROR : " + ex.Message);
+                }
+            })
+        );
+        return tasks;
+    }
+}
