f5d1746b4a70fc33b1f925ae6d9aa3a1f0bdcbf0
ghardashpour-m <ghardashpour@utravs.com>
2025-07-18 20:55:11 +0330

    feat: add `Products` endpoint

---
+++ src/ConcurrencyChallenger.Api/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using ConcurrencyChallenger.Api.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConcurrencyChallenger.Api.Controllers;
+
+[ApiController]
+[Route("api/products")]
+public class ProductController(AppDbContext db, ILogger<ProductController> logger) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetCapacityAsync(int productId)
+    {
+        var product = await db.Products.FindAsync(productId);
+        if (product == null)
+            return NotFound();
+
+        logger.LogInformation("Capacity is {Capacity}", product.Stock);
+
+        return Ok(product.Stock);
+    }
+}
